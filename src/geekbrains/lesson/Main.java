13e2cdefab6f595f18f67e45759b4612a1b62b0a
maks6169 <maks6169@rambler.ru>
2021-05-05 11:05:31 +0300

    Lesson2

--- src/geekbrains/lesson/Main.java
+++ src/geekbrains/lesson/Main.java
@@ -3,5 +3,12 @@ package geekbrains.lesson;
 public class Main {
     public static void main(String[] args) {
 
-    }
+            String[][] arr = {
+                    {"10", "20", "30", "40"},
+                    {"50", "60", "70", "80"},
+                    {"90", "10", "11", "12"},
+                    {"13", "14", "15", "16"}
+            };
+      new MyArraySizeException().ArraySizeException(arr);
+        }
     }
--- src/geekbrains/lesson/MyArraySizeException.java
+++ src/geekbrains/lesson/MyArraySizeException.java
@@ -1,6 +1,14 @@
 package geekbrains.lesson;
 
 public class MyArraySizeException extends RuntimeException {
-
-    
+  public void   ArraySizeException(String[][] arr) {
+        if (arr.length != 4) {
+            System.out.println("MyArraySizeException");
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                System.out.println("MyArraySizeException");
+            }
+        }
+    }
 }
